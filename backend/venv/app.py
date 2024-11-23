84c23944dd7d97f36a0e18e252b4d43bb3ff7971
owlyoungish349 <alirezakasiri481@gmail.com>
2024-11-23 13:50:46 +0000

    Initial commit: Added backend and frontend

---
+++ backend/venv/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+import requests
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/api/countries', methods=['GET'])
+def get_countries():
+    url = "https://countriesnow.space/api/v0.1/countries/population/cities"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return jsonify(response.json())
+    else:
+        return jsonify({"error": "Failed to fetch data"}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
